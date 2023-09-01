56cd396b3bfd2d2f61a55a5c98c2108fdf03cb54
Caleb <hamoncal25@7hills.org>
2023-09-01 12:16:50 -0400

    10 Minutes In

--- Receipt.java
+++ Receipt.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class Receipt {
     public static void main (String[] args){
-        System.out.print("Hello World");
-        System.out.print("And Twyford");
+        System.out.println("Welcome to the Crazy Cafe");
+        System.out.println("What Would You Like?");
+        System.out.println("We Currently Have The Following:");
+        System.out.println("Coffee w/ 1lb of Caffeine: $12.99");
+        System.out.println("Tea w/ 1lb of Caffeine: $10.25");
+        System.out.println("Bagel w/ 1lb of Caffeine Infused: $4.99");
+        System.out.println("Donut w/ 1lb of Caffeine Infused: $4.99");
+        System.out.println("Water: $0.99");
+
     }
 
 }
